425d18c1858a912434c971884b8e7b3eedd8243f
Reskipiandari <reskipiandari@gamil.com>
2024-06-12 16:23:39 +0800

    eky

---
+++ latihan.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+    <style>
+        table {
+            width: 100%;
+            max-width: 600px; /* Set a maximum width for alignment */
+            border-collapse: collapse;
+            margin-bottom: 20px; /* Add space between tables */
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            padding: 8px;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+    <h2>Data Mahasiswa</h2>
+    <table>
+        <tr>
+            <th>NO</th>
+            <th>NIM</th>
+            <th>Nama</th>
+            <th>Jurusan</th>
+            <th>Email</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>TII6190020</td>
+            <td>Samsul Hadi</td>
+            <td>Teknik Informatika</td>
+            <td>samsulhadi@gmail.com</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>TII6190011</td>
+            <td>Januardi Putra</td>
+            <td>Teknik Informatika</td>
+            <td>januardiputra@gmail.com</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>TII6190025</td>
+            <td>M. Izzudin</td>
+            <td>Teknik Informatika</td>
+            <td>m-izzudin@gmail.com</td>
+        </tr>
+		<tr>
+		<td>4</td>
+            <td>TII6190025</td>
+            <td>M. Izzudin</td>
+            <td>Teknik Informatika</td>
+            <td>m-izzudin@gmail.com</td>
+			</tr>
+		<tr>
+		<td>5</td>
+            <td>TII6190025</td>
+            <td>M. Izzudin</td>
+            <td>Teknik Informatika</td>
+            <td>m-izzudin@gmail.com</td>
+    </table>
+
+    
